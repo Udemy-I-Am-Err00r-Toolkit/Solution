7fed09b32073ce16f4d13497eb290a8ca62e5d66
I_Am_Err00r <brent@err00rfree.com>
2021-11-04 14:45:34 -0500

    Ability Pickup Script
    Forgot to save the edits I made while rerecording the ability pickup script to the final solution, now matches what is shown in video

--- Assets/Scripts/Stats/Items/AbilityPickup.cs
+++ Assets/Scripts/Stats/Items/AbilityPickup.cs
@@ -7,14 +7,16 @@ namespace MetroidvaniaTools
     //This script is specific to Ability prefabs that the Player would pickup to gain an ability
     public class AbilityPickup : MonoBehaviour
     {
+        protected Character character;
         public ItemType item;
         //This bool will determine if the pickup has been found by Player, if found, it will disable the game object in the scene at start
         protected bool found;
 
         private void OnEnable()
         {
+            character = FindObjectOfType<Character>();
             //Sets the found bool based on the PlayerPrefs; this is only set when the item trigger collider is interacted with the Player
-            found = PlayerPrefs.GetInt(" " + PlayerPrefs.GetInt("GameFile") + item.name) == 1 ? true : false;
+            found = PlayerPrefs.GetInt(" " + character.gameFile + item.name) == 1 ? true : false;
             if (found)
             {
                 Destroy(gameObject);
